33a256bcc349ca99f59aa9494b786bad0c1ffd4c
RakeshSR10 <rakeshsrking@gmail.com>
2021-03-31 11:11:04 +0530

    [Rakesh] modified and do compareTo method in Cartesian.java

--- Cartesian.java
+++ Cartesian.java
@@ -3,7 +3,7 @@ public class Cartesian
 	public static void main(String[] args)
 	{
 		int x1=3, x2=8, y1=5, y2=10;
-		int x3=8, x4=8, y3=5, y4=20;
+		int x3=3, x4=8, y3=5, y4=10;
 
 
 		double length=Math.sqrt( ((x2-x1)^2) + ((y2-y1)^2) );
@@ -16,6 +16,11 @@ public class Cartesian
 			System.out.println("Both lines are Equal");
 		else
 			System.out.println("Not equal");
+
+
+		String s1=String.valueOf(length);
+		String s2=String.valueOf(length1);
+		System.out.println("Result of Two lengths = "+s1.compareTo(s2));
 
 	}
 }
